f20148f2173c78e5971037efda238d3c7bb8b75b
Jim Paine <jim@jimpaine.io>
2020-05-25 21:56:59 +0100

    add running of distro setup

--- scripts/wsl2.ps1
+++ scripts/wsl2.ps1
@@ -1,2 +1,9 @@
 wsl --set-default-version 2
-winget install Canonical.Ubuntu
+winget install Canonical.Ubuntu --version 18.04
+$ubuntuApp = Get-StartApps | Where-Object {$_.Name -eq 'Ubuntu 18.04'} 
+$ubuntuApp = "shell:AppsFolder\" + $ubuntuApp.AppID
+Write-Output $ubuntuApp
+Start-Process -FilePath $ubuntuApp
+
+Write-Host "Press any key once you have setup WSL distro"
+Read-Host
