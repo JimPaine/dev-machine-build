fe4b18ccedb884429d0482d15e603b7e88506aff
Jim Paine <jim@jimpaine.io>
2021-01-04 10:27:39 +0000

    Update wsl2.ps1

--- scripts/wsl2.ps1
+++ scripts/wsl2.ps1
@@ -1,6 +1,6 @@
 wsl --set-default-version 2
 winget install Canonical.Ubuntu --version 20.04
-$ubuntuApp = Get-StartApps | Where-Object {$_.Name -eq 'Ubuntu 20.04'} 
+$ubuntuApp = Get-StartApps | Where-Object {$_.Name -eq 'Ubuntu 20.04 LTS'} 
 $ubuntuApp = "shell:AppsFolder\" + $ubuntuApp.AppID
 Write-Output $ubuntuApp
 Start-Process -FilePath $ubuntuApp
@@ -8,5 +8,5 @@ Start-Process -FilePath $ubuntuApp
 Write-Host "Press any key once you have setup WSL distro"
 Read-Host
 
-wsl --set-default Ubuntu-20.04
+wsl --set-default Ubuntu-20.04-LTS
 wsl --exec sudo ./wsl2.sh
